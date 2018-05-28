60ba79fbc3c9614b7ad6fae32546958e90d0207f
Milan Sacher <milan.lanlada@gmail.com>
2018-05-28 05:17:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,41 @@
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+
+function add(a, b) {
+  return a + b
+}
+
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+
+function subtract(a, b) {
+  return a - b
+}
+
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+
+function multiply(a, b) {
+  return a * b
+}
+
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+
+function divide(a, b) {
+  return a / b
+}
+
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+
+function inc(a) {
+  return a += 1
+}
+
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+
+function dec(a) {
+  return a-= 1
+}
