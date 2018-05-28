78496c08835dd5950e4adb09d61dc8967c90496b
Milan Sacher <milan.lanlada@gmail.com>
2018-05-28 05:27:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,4 +57,6 @@ function makeInt(a) {
 var a = Math.floor(Math.random() * 1000)
 var b = Math.floor(Math.random() * 1000)
 
-function isNaN(makeInt())
+function isNaN() {
+  if makeInt('sldkjflksjf')
+}
